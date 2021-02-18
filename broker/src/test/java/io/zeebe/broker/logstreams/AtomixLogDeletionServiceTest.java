25396886f6a589638dab58ac9a41bd9547d22ab2
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2021-02-18 08:49:39 +0100

    chore(broker): linting

--- broker/src/test/java/io/zeebe/broker/logstreams/AtomixLogDeletionServiceTest.java
+++ broker/src/test/java/io/zeebe/broker/logstreams/AtomixLogDeletionServiceTest.java
@@ -10,10 +10,8 @@ package io.zeebe.broker.logstreams;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.atomix.raft.partition.impl.RaftNamespaces;
 import io.atomix.raft.storage.RaftStorage;
 import io.atomix.raft.storage.log.RaftLogReader;
-import io.atomix.raft.zeebe.ZeebeEntry;
 import io.atomix.storage.journal.Indexed;
 import io.atomix.storage.journal.JournalSegmentDescriptor;
 import io.zeebe.logstreams.util.AtomixLogStorageRule;
